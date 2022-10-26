c8bffe9a50f9f725c942dcd74d56ef5cc04eb6a7
SirJohanot <pavelkrafft@gmail.com>
2022-10-26 12:17:15 +0300

    initial commit

---
+++ google-translate/src/main/java/com/innowise/googletranslate/pageobject/GoogleTranslatePageObject.java
@@ -0,0 +1,14 @@
+package com.innowise.googletranslate.pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class GoogleTranslatePageObject {
+
+    private final WebDriver driver;
+
+    public GoogleTranslatePageObject(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    
+}
