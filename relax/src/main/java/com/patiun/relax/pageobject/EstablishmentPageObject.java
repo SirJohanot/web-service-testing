6be7d41dc33c5b61dc2ddcb6c1da70f54c54558d
unknown <pavelkrafft@gmail.com>
2022-11-06 10:59:01 +0300

    fixed google translate text; made establishment test more thorough by asserting the working hours of each of the days of the week

--- relax/src/main/java/com/patiun/relax/pageobject/EstablishmentPageObject.java
+++ relax/src/main/java/com/patiun/relax/pageobject/EstablishmentPageObject.java
@@ -5,19 +5,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class EstablishmentPageObject extends AbstractPageObject {
 
+    private static final By ACCEPT_COOKIES_BUTTON_BY = By.xpath("//div[@class='CookiesNotification__control']");
     private static final By PHONE_NUMBER_BY = By.xpath("//a[@class='PhoneLink']");
     private static final By ADDRESS_BY = By.xpath("//button[contains(@class, 'PersonalContacts__address')]");
-    private static final By WORKING_HOURS_BY = By.xpath("//button[contains(@class, 'PersonalContacts__worktime')]/span");
-    private static final By TAKEOUT_ELEMENT_BY = By.xpath("//div[@class='PersonalDelivery__block']");
+    private static final By SHOW_WORKING_HOURS_BUTTON_BY = By.xpath("//button[contains(@class, 'PersonalContacts__worktime')]/span");
+    private static final By CLOSE_WORKING_HOURS_BUTTON_BY = By.xpath("//div[@class='TouchIcon Popup__close']");
+    private static final By WORKING_HOUR_BY = By.xpath("/html/body/div[7]/div/div[3]/div/div[1]/div[1]/main/div//div[@class='ContactsPopupOpening__subMain']");
     private static final By DISTRICT_ELEMENT_BY = By.xpath("//div[@class='ContentBox__content Features__content']//span[text()='Район']/../../../div[2]/div/span/span");
     private static final By SHOW_MORE_BUTTON_BY = By.xpath("//div[@class='ContentBox Features isVisible']//div[@class='ContentBox__showMore Link Link--primary']");
     private static final By CUISINE_ELEMENT_BY = By.xpath("//div[@class='ContentBox__content Features__content']//span[text()='Кухня']/../../../div[2]/div/span/a");
 
     public EstablishmentPageObject(WebDriver driver) {
         super(driver);
+        clickButton(ACCEPT_COOKIES_BUTTON_BY);
     }
 
     public String getPhoneNumber() {
@@ -30,14 +36,18 @@ public class EstablishmentPageObject extends AbstractPageObject {
         return addressElement.getAttribute("title");
     }
 
-    public String getWorkingHours() {
-        WebElement workHoursElement = getElementWhenVisible(WORKING_HOURS_BY);
-        return workHoursElement.getText();
-    }
+    public List<String> getWorkingHours() {
+        clickButton(SHOW_WORKING_HOURS_BUTTON_BY);
+        WebElement closeWorkingHoursButton = getElementWhenClickable(CLOSE_WORKING_HOURS_BUTTON_BY);
 
-    public boolean hasTakeout() {
-        List<WebElement> takeoutBlock = driver.findElements(TAKEOUT_ELEMENT_BY);
-        return !takeoutBlock.isEmpty();
+        List<WebElement> workHoursElements = driver.findElements(WORKING_HOUR_BY);
+        List<String> workingHours = workHoursElements.stream()
+                .map(WebElement::getText)
+                .toList();
+
+        closeWorkingHoursButton.click();
+
+        return workingHours;
     }
 
     public String getDistrict() {
